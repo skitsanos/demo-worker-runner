989940e0e8c0f615ba3a1815cdae7499c9e601a3
skitsanos <info@skitsanos.com>
2022-06-09 10:08:27 +0300

    chore: cleanup

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ const downloadFileAsync = require('./downloadFileAsync');
 console.log('Calling download file, the old way');
 downloadFile('oldway.file', {id: -999});
 
-//downloadFileAsync('https://api.skitsanos.com/api/utils/headers').then(console.log);
+downloadFileAsync('https://api.skitsanos.com/api/utils/headers?the_old_way=yes').then(console.log);
 
 console.log('Downloading threaded');
-const rawTasks = Array.from({length: 10}, (_, id) => ({
+const rawTasks = Array.from({length: 100}, (_, id) => ({
     path: './downloadFile',
     workerData: {options: {id}, url: `url...#${id}`}
 }));
@@ -22,7 +22,7 @@ for (const worker of rawTasks)
 WorkerRunner.add({
     path: './downloadFileAsync',
     workerData: {
-        url: 'https://api.skitsanos.com/api/utils/headers'
+        url: 'https://api.skitsanos.com/api/utils/headers?the_new_way=true'
     }
 });
 
